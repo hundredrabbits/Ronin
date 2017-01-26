7ffea7ac30bbd2712ad9ad37412ffdfd3ce9fac6
Devine Lu Linvega <aliceffekt@gmail.com>
2017-01-26 11:45:10 -0700

    Export with linecap

--- scripts/modules/vector.js
+++ scripts/modules/vector.js
@@ -52,7 +52,7 @@ function Vector(rune)
   {
     var s = "";
 
-    s += "<svg width='"+ronin.surface.size.width+"' height='"+ronin.surface.size.height+"' xmlns='http://www.w3.org/2000/svg' baseProfile='full' version='1.1' style='fill:none;stroke:red;stroke-width:2px'>";
+    s += "<svg width='"+ronin.surface.size.width+"' height='"+ronin.surface.size.height+"' xmlns='http://www.w3.org/2000/svg' baseProfile='full' version='1.1' style='fill:none;stroke:red;stroke-width:2px;stroke-linecap:square;'>";
 
     for (var i = 0; i < this.paths.length; i++) {
       s += "<path d='"+this.paths[i]+"' />";
