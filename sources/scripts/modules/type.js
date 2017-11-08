c2dd66204a7e657b50f4deaec36a965e29e301e4
Devine Lu Linvega <aliceffekt@gmail.com>
2017-11-08 17:01:02 +1300

    Added text anchor

--- sources/scripts/modules/type.js
+++ sources/scripts/modules/type.js
@@ -2,18 +2,17 @@ function Type()
 {
   Module.call(this,"type");
 
-  this.settings = {color:"#000000",font:"Arial"}
+  this.settings = {color:"#000000",font:"Arial",anchor:"center"}
 
   this.methods.write = new Method("write","text&x,y|WxH","Draw text",function(q){
-
-    ronin.preview.clear();
 
     var rect = q[1];
     var size = rect.height * 2
     ronin.preview.clear();
 
+    ronin.render.context().textAlign = ronin.type.settings.anchor;
     ronin.render.context().font = size+"px "+ronin.type.settings.font;
-    ronin.render.context().fillText(q[0],rect.x * 2,(rect.y * 2)+size);
+    ronin.render.context().fillText(q[0].replace("+"," "),rect.x * 2,(rect.y * 2)+size);
   })
 
   this.preview = function(q)
@@ -25,7 +24,8 @@ function Type()
     
     ronin.preview.clear();
 
+    ronin.preview.context().textAlign = this.settings.anchor;
     ronin.preview.context().font = size+"px "+this.settings.font;
-    ronin.preview.context().fillText(q.methods.write[0],rect.x * 2,(rect.y * 2)+size);
+    ronin.preview.context().fillText(q.methods.write[0].replace("+"," "),rect.x * 2,(rect.y * 2)+size);
   }
 }
