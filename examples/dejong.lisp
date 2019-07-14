70ac8f2aa7e5e606c1727e086bd58b5d057832de
Quentin Leonetti <q.leonetti@gmail.com>
2019-07-14 07:46:04 +0200

    updated dejong example

--- examples/dejong.lisp
+++ examples/dejong.lisp
@@ -1,11 +1,11 @@
 (
   (clear)
-  (def point (lambda (x y color) (stroke (circle x y 1) 1 color)))
+  (def point (lambda (x y color) (stroke (circle x y 0.1) 1 color)))
   (def _dejong (lambda (x y a b c d)
     (rest ((point 
-      (add 200 (mul 50 x))
-      (add 200 (mul 50 y))
-      "red")
+      (add 700 (mul 100 x))
+      (add 400 (mul 100 y))
+      "rgba(255,0,0,1)")
     (add (sin (mul a y)) (mul x (cos (mul b x))))
     (add (mul x (sin (mul x c))) (cos (mul d y)))
     ))
@@ -22,6 +22,5 @@
       (2 1)
     )
   ))
-
-  (dejong 4000 1.4 -2.3 2.4 -2.1)
+  (dejong 32000 1.4 -2.3 2.4 -2.1)
 )
