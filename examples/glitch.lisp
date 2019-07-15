2e4dcf08d8efd560bccb42aaa8766b15a838b5ce
Quentin Leonetti <q.leonetti@gmail.com>
2019-07-15 22:11:39 +0200

    add glitch example

---
+++ examples/glitch.lisp
@@ -0,0 +1,24 @@
+; pixels
+
+(
+  (clear)
+
+  ; Filter
+
+  (defn glitch 
+    (rec) 
+    (if (gt rec 1)
+    ((clone 
+      (rect (random 400) (random 400) 2 2)
+      (rect (random 400) (random 400) 
+(random 10) (random 30)))
+      (glitch (sub rec 1))))
+  )
+
+  ; Draw photo 
+  
+  (draw 
+    "../static/crystal.jpg" 
+    (rect 0 0 400 400) 
+    (lambda () (glitch 2000)))
+)
