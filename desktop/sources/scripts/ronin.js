06bfca5828e70ae68c43eba0e8155e3a3e361f6a
Devine Lu Linvega <aliceffekt@gmail.com>
2019-07-17 11:17:27 +0900

    Fixed animate example

--- desktop/sources/scripts/ronin.js
+++ desktop/sources/scripts/ronin.js
@@ -61,6 +61,7 @@ function Ronin () {
   }
 
   this.animate = (b = true) => {
+    if(this.always === b){ return }
     this.always = b
     this.commander.run()
   }
--- examples/animate.lisp
+++ examples/animate.lisp
@@ -1,10 +1,13 @@
 ; animate
 
 (
-(ronin animate)
-(def t (sin (div (time) 100)))
+  (def start (get ronin "animate"))
+  
+  (def t (sin (div (time) 100)))
 
-(def pos (add 200 (mul 30 t)))
-(defn square (a) (rect a a a a))
-(stroke (square pos) 1 "red")
+  (def pos (add 200 (mul 30 t)))
+  (defn square (a) (rect a a a a))
+  (stroke (square pos) 1 "red")
+
+  (start)
 )
