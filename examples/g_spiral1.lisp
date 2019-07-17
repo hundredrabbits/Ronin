01f387f8e6930769422b26dc5e35d408adc3caaf
ngradwohl <guru@local-guru.net>
2019-07-17 06:17:17 +0200

    added my "Ronin experiment no 1 - spiral"

---
+++ examples/g_spiral1.lisp
@@ -0,0 +1,24 @@
+(
+ (defn rec
+  (v)
+  (if (gt v 0)
+   ((stroke 
+     (circle 
+      (add 300 
+       (mul (cos (add  (div v 17) (div (time) 2000))) 
+        (div v 2)
+       )
+      )
+      (add 300 
+       (mul (sin (div v 11)) 
+        (div v 2)
+       )
+      )
+      (div v 2))
+     1 "rgba(255,255,255,0.1")
+    (rec (sub v 0.3))
+   )
+  )
+ )
+(rec 300)
+)
