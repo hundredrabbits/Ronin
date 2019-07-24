33574979cd2c911c360bf3e2fba669bea2a6bbe1
Devine Lu Linvega <aliceffekt@gmail.com>
2019-07-24 15:48:13 +0900

    Cleaner fog

--- examples/demo/fog.lisp
+++ examples/demo/fog.lisp
@@ -1,28 +1,28 @@
+;
 (resetTransform)
 (clear)
-
+;
 (def w (of (frame) :c))
 (def h (of (frame) :m ))
-
-(defn mouse-pos (pos))
-(defn prev-pos (pos))
-
-(defn update (e) (
+(def mouse-pos (pos))
+(def prev-pos (pos))
+;
+(defn when-mouse-move (e) (
   (set mouse-pos :x (of e :x))
   (set mouse-pos :y (of e :y))
 ))
-
-(on "animate" '(
-  (stroke (line prev-pos mouse-pos) 4 "#fff")
+;
+(defn when-animate () (
+  (stroke (line prev-pos mouse-pos) 4 "#72dec2")
   (move w h)
   (rotate 0.002)
   (scale 0.998)
-  
   (translate (frame) (pos (mul -1 w) (mul -1 h)))
   (resetTransform)
   (fill (frame) "#00000004")
   (set prev-pos :x (of mouse-pos :x))
   (set prev-pos :y (of mouse-pos :y))
 ))
-
-(on "mouse-move" update)
+;
+(on "animate" when-animate)
+(on "mouse-move" when-mouse-move)
