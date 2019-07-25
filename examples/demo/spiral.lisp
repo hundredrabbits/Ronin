25c106ee24ffeca8e61279e455600c0be371fe39
Quentin Leonetti <q.leonetti@gmail.com>
2019-07-25 18:13:26 +0200

    update spiral example

--- examples/demo/spiral.lisp
+++ examples/demo/spiral.lisp
@@ -5,8 +5,8 @@
   (if (gt v 0) 
     (
       ; params
-      (def spiral-x (add (of (frame) :c) (mul (cos (add (div v 17) (time 0.001))) (div v 2))))
-      (def spiral-y (add (of (frame) :m) (mul (sin (div v 11)) (div v 2))))
+      (def spiral-x (add (:c (frame)) (mul (cos (add (div v 17) (time 0.001))) (div v 2))))
+      (def spiral-y (add (:m (frame)) (mul (sin (div v 11)) (div v 2))))
       (def spiral-r (div v 2))
       ; draw
       (stroke (circle spiral-x spiral-y spiral-r) 1 "rgba(114,222,194,0.1)") (rec (sub v 0.3))))) 
