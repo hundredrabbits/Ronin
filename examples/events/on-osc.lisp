b30b3eb2d822d30b06460123f567e96ea83abffc
Devine Lu Linvega <aliceffekt@gmail.com>
2019-07-26 13:09:54 +0900

    Updated OSC example

--- examples/events/on-osc.lisp
+++ examples/events/on-osc.lisp
@@ -1,1 +1,23 @@
-(on "/a" (λ (msg) (debug msg)))
+;
+(def frame-rect 
+  (frame))
+(def disc 
+  (circle frame-rect:c frame-rect:m 200))
+;
+(defn on-animate 
+  () 
+  (
+    (clear) 
+    (fill disc "#72dec2") 
+    (set disc "r" 
+      (clamp 
+        (mul disc:r 0.95) 0 200))))
+;
+(defn on-osc 
+  (msg) 
+  (
+    (echo msg) 
+    (set disc "r" 200)))
+; triggers
+(on "animate" on-animate)
+(on "/a" on-osc)
