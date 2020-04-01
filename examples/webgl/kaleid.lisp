561429152c8337e6fea97300b86a04664dc01947
David Minnix <dlminnix09@gmail.com>
2020-04-01 17:08:13 -0400

    WIP - got basic shaders working using examplecode from https://webglfundamentals.org/webgl/lessons/webgl-image-processing.html

--- examples/webgl/kaleid.lisp
+++ examples/webgl/kaleid.lisp
@@ -1,27 +1,8 @@
-(import $path)
+(import "IMG_0097.JPG")
 
-(runshader (fragmentshader `
-    precision highp float;
+;this all has to be on one line so that 
+;the parser will take it as one arg
+;im sure it would be possible to introduce 
+;backticks for multiline strings
 
-    // our texture
-    uniform sampler2D u_image;
-    
-    // the texCoords passed in from the vertex shader.
-    varying vec2 v_texCoord;
-
-    vec2 kaleid(vec2 st, float nSides){
-      // st -= 0.5;
-      float r = length(st);
-      float a = atan(st.y, st.x);
-      float pi = 2.0*3.1416;
-      a = mod(a,pi/nSides);
-      a = abs(a-pi/nSides/2.0);
-      return r*vec2(cos(a), sin(a));
-    }
-    
-    void main() {
-       vec2 coord = kaleid(v_texCoord,5.0);
-       gl_FragColor = texture2D(u_image, coord);
-    }
-
-  `) (vertexshader) (rect 0 0 600 600))
+(runshader (fragmentshader "precision highp float;uniform sampler2D u_image;varying vec2 v_texCoord;vec2 kaleid(vec2 st, float nSides){float r = length(st);float a = atan(st.y, st.x);float pi = 2.0*3.1416;a = mod(a,pi/nSides);a = abs(a-pi/nSides/2.0);return r*vec2(cos(a), sin(a));}void main() {vec2 coord = kaleid(v_texCoord,5.0);gl_FragColor = texture2D(u_image, coord);}") (vertexshader) (rect 0 0 600 600))
