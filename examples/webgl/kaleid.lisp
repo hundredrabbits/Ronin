419b99dd26776e934f82662fb0a4943b3e151669
David Minnix <dlminnix09@gmail.com>
2020-04-01 22:41:00 -0400

    moved gl code to its own module

--- examples/webgl/kaleid.lisp
+++ examples/webgl/kaleid.lisp
@@ -1,3 +1,4 @@
+(clear)
 (import $path)
 
 ;this all has to be on one line so that 
@@ -5,4 +6,5 @@
 ;im sure it would be possible to introduce 
 ;backticks for multiline strings
 
-(runshader (fragmentshader "precision highp float;uniform sampler2D u_image;varying vec2 v_texCoord;vec2 kaleid(vec2 st, float nSides){float r = length(st);float a = atan(st.y, st.x);float pi = 2.0*3.1416;a = mod(a,pi/nSides);a = abs(a-pi/nSides/2.0);return r*vec2(cos(a), sin(a));}void main() {vec2 coord = kaleid(v_texCoord,5.0);gl_FragColor = texture2D(u_image, coord);}") (vertexshader) (rect 0 0 600 600))
+(runshader (fragmentshader "precision highp float;uniform sampler2D u_image;varying vec2 v_texCoord;vec2 kaleid(vec2 st, float nSides){st-=0.5;float r = length(st);float a = atan(st.y, st.x);float pi = 2.0*3.1416;a = mod(a,pi/nSides);a = abs(a-pi/nSides/2.0);return r*vec2(cos(a), sin(a));}void main() {vec2 coord = kaleid(v_texCoord,5.0);gl_FragColor = texture2D(u_image, coord);}"))
+
