7204a52e7cbcb6c29a0066efba4668e6fc9b746e
neauoire <aliceffekt@gmail.com>
2020-04-07 11:40:52 +0900

    Added svg example

---
+++ examples/projects/svg.lisp
@@ -0,0 +1,5 @@
+(def points 
+  (add "0,100 " "50,25 " "50,75 " "100,0"))
+
+(print 
+  (add "<?xml version='1.0' encoding='iso-8859-1'?><svg version='1.1' id='Capa_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve'><polyline points='" points "' /></svg>"))
